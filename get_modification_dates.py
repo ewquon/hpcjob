0d475893c7f131ad545e54c96a4ccd9327d97ef2
Eliot Quon <eliot.quon@nrel.gov>
2018-01-23 14:20:20 -0700

    Check oldest time stamp for each file/directory within specified  path(s)

---
+++ get_modification_dates.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import os
+import datetime
+
+def timestring(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+
+for dpath in sys.argv[1:]:
+    fullpaths = []
+    timestamps = []
+    for fpath in os.listdir(dpath):
+        fullpaths.append(os.path.join(dpath,fpath))
+        timestamps.append(os.path.getmtime(fullpaths[-1]))
+        #print(fpath,timestring(timestamps[-1]))
+    timestamps_day = [int(tstamp/3600./24.)*3600.*24. for tstamp in timestamps]
+    unique_days = list(set(timestamps_day))
+    unique_days.sort(reverse=True)
+    print(dpath,'unique times:\n',
+            [ timestring(tstamp) for tstamp in unique_days ])
