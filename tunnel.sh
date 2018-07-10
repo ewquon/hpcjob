290b23b2256c954e31cd5cba36d492e538a8ffe6
Eliot Quon <eliot.quon@nrel.gov>
2018-07-10 10:09:28 -0600

    Add script to set up SSH tunnel to HPC DAV nodes

---
+++ tunnel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -z "$2" ]; then
+    echo 'Run remote: jupyter notebook --ip="*" --no-browser'
+    echo "USAGE: $0 local_port remote_port [remote_addr]"
+    exit
+fi
+remoteaddr='hpc-dav4'
+
+if [ -n "$3" ]; then
+    remoteaddr="$3"
+fi
+
+ssh -L $1:$remoteaddr:$2 -f -N peregrine.hpc.nrel.gov
