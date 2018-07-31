99cf997b9471689eb06ffb00c8cc3587426760dd
Eliot Quon <eliot.quon@nrel.gov>
2018-07-31 11:59:18 -0600

    Add more helpful screen output

--- checkpurge.sh
+++ checkpurge.sh
@@ -6,13 +6,18 @@ cd $prefix
 datestr=`date +%Y%m%d`
 
 if [ -z "$1" ]; then
+    echo 'Endangered list:'
     cat $prefix/.notify-list
+    echo ''
+    echo "USAGE: $0 [path1] [path2] ..."
 else
     for path in $*; do
         pathshort=${path#$prefix/}
         echo ''
         echo "Looking for files older than $atime days in $pathshort"
         pathshort=${pathshort//\//.}
-        time lfs find $path -type f --atime +$atime > $prefix/log.checkpurge.$pathshort.$datestr
+        fname="$prefix/log.checkpurge.$pathshort.$datestr"
+        time lfs find $path -type f --atime +$atime > $fname
+        echo "Log in $fname"
     done
 fi
