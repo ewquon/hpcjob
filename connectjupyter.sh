c9e26528c6c536855020e5fc7f4a1e71d82fecf5
Eliot Quon <eliot.quon@nrel.gov>
2018-10-17 19:05:02 -0600

    Use remoteaddr in place of remotename; remoteaddr is hard-coded.

--- connectjupyter.sh
+++ connectjupyter.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
-remoteaddr='hpc-dav4'
+#
+# Script to facilitate running remote Jupyter notebook servers and
+# creating ssh tunnels. 
+#
+# written by Eliot Quon (eliot.quon@nrel.gov)
+#
+# Start remote server (e.g., within FastX session) with:
+#
+#   jupyter notebook --ip="*" --no-browser
+#
+# USAGE:
+#
+#   # To display list of running servers
+#   connectjupyter.sh
+#
+#   # To create tunnel to existing server and open up notebook server
+#   # in a local browser; the local port number is the remote number 
+#   # +1000, to prevent confusion with locally started notebook servers
+#   connectjupyter.sh <remote_port_number>
+#
+remoteaddr=dav4.hpc.nrel.gov
 
 get_address()
 {
+    connected=false
     while read -r line || [[ -n "$line" ]]; do
         if [ -z "$1" ]; then
             echo $line
@@ -15,18 +36,20 @@ get_address()
             if [ "$port" == "$1" ]; then
                 localport=$((port+1000))
                 echo "Mapping $localport to $addr ($path)"
-                ssh -L $localport:$remoteaddr:$port -f -N peregrine.hpc.nrel.gov
+                ssh -L $localport:$remoteaddr:$port -f -N peregrine.hpc.nrel.gov 2>/dev/null
                 if [ "$?" == 0 ]; then
-                    localaddr=`echo $remoteaddr | sed "s/$port/$localport/"`
+                    connected=true
+                    localaddr=`echo $addr | sed "s/$port/$localport/"`
+                    echo "Opening $localaddr"
                     open $localaddr
                 fi
             fi
         fi
     done
+    if [ -n "$1" -a "$connected" = false ]; then
+        echo "Failed to connect to port $1 on $remoteaddr" 
+    fi
 }
 
-if [ -n "$2" ]; then
-    remoteaddr="$2"
-fi
 ssh $remoteaddr 'jupyter notebook list' 2>/dev/null | get_address $1
 
--- tunnel.sh
+++ tunnel.sh
@@ -4,7 +4,8 @@ if [ -z "$2" ]; then
     echo "USAGE: $0 local_port remote_port [remote_addr]"
     exit
 fi
-remoteaddr='hpc-dav4'
+#remoteaddr='hpc-dav4'
+remoteaddr='dav4.hpc.nrel.gov'
 
 if [ -n "$3" ]; then
     remoteaddr="$3"
