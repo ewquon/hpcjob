b884f0304a707645a0218545af8958554fa72d8b
Eliot Quon <eliot.quon@nrel.gov>
2018-10-22 15:57:33 -0600

    Handle addresses w/o specified token
    Check if tunnel exists and connect to the correct local port (not
    necessarily remote+1000); if creating a tunnel failed, then print out
    the ssh command for debugging.

--- connectjupyter.sh
+++ connectjupyter.sh
@@ -9,6 +9,11 @@
 #
 #   jupyter notebook --ip="*" --no-browser
 #
+# Assumes that the ssh tunnel command is of the form:
+#
+#   ssh -L $local_port:$remoteaddr:$remote_port -f -N $domain
+#
+#
 # USAGE:
 #
 #   # To display list of running servers
@@ -20,10 +25,14 @@
 #   connectjupyter.sh <remote_port_number>
 #
 remoteaddr=dav4.hpc.nrel.gov
+domain=peregrine.hpc.nrel.gov
+opencmd='open'  # Mac OSX command to open browser
 
 get_address()
 {
     connected=false
+    token=''
+    cmd=''
     while read -r line || [[ -n "$line" ]]; do
         if [ -z "$1" ]; then
             echo $line
@@ -32,28 +41,51 @@ get_address()
             addr=${line%% ::*}
             path=${line##*:: }
             port=${addr#http://localhost:}
-            port=${port%/\?*}
+            if [[ $port == *token=* ]]; then
+                # have token (instead of notebook password)
+                token=${port#*/?token=}
+            fi
+            port=${port%/*} # remote port
             if [ "$port" == "$1" ]; then
-                localport=$((port+1000))
-                cmd="ssh -L $localport:$remoteaddr:$port -f -N peregrine.hpc.nrel.gov"
-                running=`ps -e | grep "$cmd" | grep -v 'grep'`
+                #cmd="ssh -L $localport:$remoteaddr:$port -f -N $domain"
+                #running=`ps -e | grep "$cmd" | grep -v 'grep'`
+                running=`ps -e | grep 'ssh -L' | grep -e $domain -e $port`
                 if [ -z "$running" ]; then
-                    echo "Mapping $localport to $addr ($path)"
+                    # create tunnel
+                    localport=$((port+1000))
+                    cmd="ssh -L $localport:$remoteaddr:$port -f -N $domain"
+                    echo "Mapping $localport to remote $addr ($path)"
                     $cmd 2>/dev/null
+                    connected="$?"
                 else
-                    echo "Tunnel from localhost:$localport to $addr already exists"
+                    echo "Tunnel to $addr already exists"
+                    connected=0
+                    # scrape the local port (tunnel may have been user created
+                    # and not necessarily equal to the remote port+1000
+                    sshparams=${running#*ssh}
+                    localport=`echo "${sshparams%%:*}" | awk '{print $(NF)}'`
                 fi
-                if [ "$?" == 0 ]; then
-                    connected=true
+                if [ "$connected" == 0 ]; then
+                    # perform local port substition and open notebook in browser
                     localaddr=`echo $addr | sed "s/$port/$localport/"`
-                    echo "Opening $localaddr"
-                    open $localaddr
+                    if [ -n "$token" ]; then
+                        echo "Opening $localaddr"
+                    else
+                        echo "Opening $localaddr without token (need to specify notebook password)"
+                    fi
+                    $opencmd $localaddr
                 fi
             fi
         fi
     done
-    if [ -n "$1" -a "$connected" = false ]; then
-        echo "Failed to connect to port $1 on $remoteaddr" 
+    if [ -n "$1" -a "$connected" != 0 ]; then
+        # We specified a port to map/connect to but somethign went wrong...
+        if [ -z "$cmd" ]; then
+            echo "No notebook server found on port $1"
+        else
+            echo "Failed to connect to port $1 on $remoteaddr with command:"
+            echo "  $cmd"
+        fi
     fi
 }
 
