081e2255db234bea1559dbd488818fe83b646b4f
Eliot Quon <eliot.quon@nrel.gov>
2019-09-26 10:48:29 -0600

    Submit srun command using subprocess

--- interactive.py
+++ interactive.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import os
+import sys,os
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--account',metavar='account',type=str,default='mmc')
@@ -8,5 +9,20 @@ parser.add_argument('--time',metavar='time',type=str,default='30')
 parser.add_argument('--nodes',metavar='nodes',type=int,default=1)
 args = parser.parse_args()
 
-print('srun --time={:s} --account={:s} --nodes={:d} --pty {:s}'.format(
-        args.time, args.account, args.nodes, os.environ['SHELL']))
+cmdlist = [
+    'srun',
+    '--time={:s}'.format(args.time),
+    '--account={:s}'.format(args.account),
+    '--nodes={:d}'.format(args.nodes),
+    '--pty', os.environ['SHELL'],
+] 
+print(' '.join(cmdlist))
+
+prompt = raw_input if (sys.version_info[0] < 3) else input
+try:
+    prompt('press enter to continue...\n')
+except KeyboardInterrupt:
+    pass
+else:
+    subprocess.Popen(cmdlist)
+
