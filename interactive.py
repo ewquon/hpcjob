1adb18d3b29a05fd66b65c377e884db3be17ad93
Eliot Quon <eliot.quon@nrel.gov>
2021-10-04 09:42:09 -0600

    Add --no-mail option

--- interactive.py
+++ interactive.py
@@ -8,6 +8,8 @@ parser.add_argument('--account',metavar='account',type=str,default='mmc')
 parser.add_argument('--time',metavar='time',type=str,default='30')
 parser.add_argument('--ntasks',metavar='ntasks',type=int,default=36)
 parser.add_argument('--qos',metavar='qos',type=str,default='normal')
+parser.add_argument('--mail',dest='mail',default=True,action='store_true')
+parser.add_argument('--no-mail',dest='mail', action='store_false')
 args = parser.parse_args()
 
 cmdlist = [
@@ -16,10 +18,13 @@ cmdlist = [
     '--account={:s}'.format(args.account),
     '--ntasks={:d}'.format(args.ntasks),
     '--qos={:s}'.format(args.qos),
-    '--mail-type=BEGIN',
-    '--mail-user=eliot.quon@nrel.gov',
-    '--pty', os.environ['SHELL'],
-] 
+]
+if args.mail:
+    cmdlist += [
+        '--mail-type=BEGIN',
+        '--mail-user=eliot.quon@nrel.gov',
+    ]
+cmdlist += ['--pty', os.environ['SHELL']]
 print(' '.join(cmdlist))
 
 prompt = raw_input if (sys.version_info[0] < 3) else input
